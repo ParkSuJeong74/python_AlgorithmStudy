efd679c191bb8fa6454dd19c0b66c02344b89f28
junhee <95354566+drybulk@users.noreply.github.com>
2021-12-08 20:29:14 +0900

    Update 1110_by_JH.py

--- 1주차-기본(1)/while문/더하기_사이클(1110)/현준희/1110_by_JH.py
+++ 1주차-기본(1)/while문/더하기_사이클(1110)/현준희/1110_by_JH.py
@@ -1,13 +1,13 @@
 count=0
-n=int(input())
-k=n
-flag=1
-while flag==1 :
-    one=k%10
-    ten=int(k/10)
-    newone=(ten+one)%10
-    k=one*10+newone
-    count+=1
-    if k==n :
+n=int(input())          # 정수 N 입력   26을 입력받았다고 가정
+k=n                   #임의의 변수 k에 N값을 저장    k->26
+flag=1            #플래그 변수 
+while flag==1 :      
+    one=k%10        #     일의자리 분리 26->6  :one=6
+    ten=int(k/10)             #십의자리 분리 26->2 :ten=2
+    newone=(ten+one)%10           # 새로운 일의자리 구하기 26->2+6 ->8      2+6(ten+one)%10=8
+    k=one*10+newone         # 6*10 + 8 -> 68                
+    count+=1                #사이클 길이 누적
+    if k==n :              #while반복문 안에서 사이클을 계속 돌다 원래 수 N이 되면 사이클 종료
         break;
-print(count)
+print(count)             #누적된 사이클 길이 출력
