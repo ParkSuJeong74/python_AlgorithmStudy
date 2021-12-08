7ea39eefea9d7b7f2d40e0c8d6ee274386bbc6b5
junhee <95354566+drybulk@users.noreply.github.com>
2021-12-08 21:26:41 +0900

    Update 1110_by_JH.py

--- 1주차-기본(1)/while문/더하기_사이클(1110)/현준희/1110_by_JH.py
+++ 1주차-기본(1)/while문/더하기_사이클(1110)/현준희/1110_by_JH.py
@@ -1,8 +1,7 @@
 count=0
 n=int(input())          # 정수 N 입력   26을 입력받았다고 가정
-k=n                   #임의의 변수 k에 N값을 저장    k->26
-flag=1            #플래그 변수 
-while flag==1 :      
+k=n                   #임의의 변수 k에 N값을 저장    k->26 
+while 1 :      
     one=k%10        #     일의자리 분리 26->6  :one=6
     ten=int(k/10)             #십의자리 분리 26->2 :ten=2
     newone=(ten+one)%10           # 새로운 일의자리 구하기 26->2+6 ->8      2+6(ten+one)%10=8
