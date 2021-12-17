5852b29689765184d86c2854c1b9c72a745e373d
ParkSuJeong <71163016+ParkSuJeong74@users.noreply.github.com>
2021-12-17 10:54:14 +0900

    #2 문자열 1316 해설

--- 2주차-기본(2)/문자열/그룹_단어_체커(1316)/박수정/1316.py
+++ 2주차-기본(2)/문자열/그룹_단어_체커(1316)/박수정/1316.py
@@ -1,17 +1,22 @@
 import sys
 
+# 그룹단어 판별 함수
 def checker(string):
+    # 한 문자씩
     for i in range(len(string)-1):
+        # 두 연속하는 문자가 같지 않고
         if string[i] != string[i+1]:
+            # 이전 문자에 이미 존재함
             if string[i+1] in string[:i]:
                 return False
-    return True
+    return True # 해당하지 않으면 그룹단어
 
 n = int(sys.stdin.readline())
 
 idx= 0
 for i in range(n):
     string = str(sys.stdin.readline())
+    # 그룹단어면 갯수 카운트
     if checker(string) == True :
         idx +=1
 print(idx)
